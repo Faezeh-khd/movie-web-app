8b28b406556e9b02cea59c0afbb8ad8f13954f0b
Faezeh Khadem <73037085+Faezeh-khd@users.noreply.github.com>
2022-11-25 13:16:03 +0330

    Adding filters

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,26 +1,57 @@
-import movieList from '../../assets/movie-list'
-const SET_SEARCH = "SET_SEARCH"
+import movieList from "../../assets/movie-list";
+const SET_SEARCH = "SET_SEARCH";
+const SET_FILTER = "SET_FILTER";
 
 const state = {
   movies: movieList,
-  search: ''
+  search: "",
+  filter: {
+    key: "rating",
+    order: "desc",
+  },
 };
 const mutations = {
-  [SET_SEARCH](state, search){
-    state.search = search
-  }
+  [SET_SEARCH](state, search) {
+    state.search = search;
+  },
+  [SET_FILTER](state, filter) {
+    state.filter = filter;
+  },
 };
 const actions = {
   search({ commit }, search) {
     commit(SET_SEARCH, search);
   },
+  filter({ commit }, filter) {
+    commit(SET_FILTER, filter);
+  },
 };
 const getters = {
   getMovies: (state) => {
-    return state.movies.filter(
-      movie => movie.name.toLowerCase().indexOf(state.search.toLowerCase()) > -1
-    );
+    return state.movies
+      .filter(
+        (movie) =>
+          movie.name.toLowerCase().indexOf(state.search.toLowerCase()) > -1
+      )
+      .sort(compare(state.filter));
   },
+};
+
+const compare = ({ key, order }) => {
+  return (a, b) => {
+    let result = 0;
+
+    if (a[key] > b[key]) {
+      result = 1;
+    }
+
+    if (a[key] < b[key]) {
+      result = -1;
+    }
+
+    if (order === "asc") return result;
+    return result * -1; //reverse
+  };
 };
 
 export default {
