403ae78d7380861fe3e2cfc5c17e8a6ffed5073d
Faezeh Khadem <fa.khadem18@gmail.com>
2023-02-03 17:32:38 +0330

    creating modal component

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,83 +1,101 @@
 <template>
   <div id="sidebar">
-	<filters/>
-	<button>Add movie</button>
+    <filters />
+    <button @click="showModal = true">Add movie</button>
+    <modal v-if="showModal" @close="showModal = !showModal" />
   </div>
 </template>
 
 <script>
-import Filters from './Filters.vue'
+import Filters from "./Filters.vue";
+import Modal from "./Modal.vue";
 export default {
-	components: {
-		Filters
-	}
-
-}
+  components: {
+    Filters,
+	Modal,
+  },
+  data() {
+    return {
+      showModal: false,
+      form: {
+        name: "",
+        year: "",
+        rating: "",
+        genre: "",
+        budget: "",
+        boxOffice: "",
+        poster: "",
+        actors: [{ name: "" }],
+        storyline: "",
+      },
+    };
+  },
+};
 </script>
 
 <style lang='scss' scoped>
-	#sidebar {
-		display: flex;
-		flex-direction: column;
-		padding: 10px;
-		min-height: 100vh;
-		min-width: 150px;
-		max-width: 150px;
-		flex-grow: 1;
-		background-color: #191c1f;
-		justify-content: left;
+#sidebar {
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  min-height: 100vh;
+  min-width: 150px;
+  max-width: 150px;
+  flex-grow: 1;
+  background-color: #191c1f;
+  justify-content: left;
 
-		& > button {
-			margin-top: 20px;
-			height: 30px;
-			background-color: #5eb85e;
-			border: none;
-			text-transform: uppercase;
-			font-weight: 600;
-			border-radius: 10px;
-			outline: none;
-			cursor: pointer;
-		}
+  & > button {
+    margin-top: 20px;
+    height: 30px;
+    background-color: #5eb85e;
+    border: none;
+    text-transform: uppercase;
+    font-weight: 600;
+    border-radius: 10px;
+    outline: none;
+    cursor: pointer;
+  }
 
-		#movie-form {
-			display: flex;
-			flex-direction: column;
-			text-align: left;
+  #movie-form {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
 
-			& > div {
-				display: flex;
-				flex-direction: column;
+    & > div {
+      display: flex;
+      flex-direction: column;
 
-				#actor-input {
-					display: flex;
-					justify-content: space-between;
-				}
-			}
+      #actor-input {
+        display: flex;
+        justify-content: space-between;
+      }
+    }
 
-			.add-actor {
-				background-color: green;
-				text-align: center;
-				color: white;
-				margin-left: 5px;
-				height: 100%;
-				padding: 2px 10px;
-				font-size: 20px;
-				padding: 1re;
-				align-self: center;
-				cursor: pointer;
-			}
-		}
+    .add-actor {
+      background-color: green;
+      text-align: center;
+      color: white;
+      margin-left: 5px;
+      height: 100%;
+      padding: 2px 10px;
+      font-size: 20px;
+      padding: 1re;
+      align-self: center;
+      cursor: pointer;
+    }
+  }
 
-		#add-movie {
-			background-color: #5eb85e;
-			border: none;
-			padding: 5px;
-			width: 70px;
-			color: white;
-			border-radius: 10px;
-			cursor: pointer;
-			text-transform: uppercase;
-			outline: none;
-		}
-	}
+  #add-movie {
+    background-color: #5eb85e;
+    border: none;
+    padding: 5px;
+    width: 70px;
+    color: white;
+    border-radius: 10px;
+    cursor: pointer;
+    text-transform: uppercase;
+    outline: none;
+  }
+}
 </style>
